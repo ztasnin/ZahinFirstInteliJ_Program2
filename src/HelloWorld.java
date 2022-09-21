d958fa686a7c47e3e5d29d391861ae216272fb82
1E10 Laptop <sample@bths.edu>
2022-09-21 12:14:53 -0400

    new class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+
+
+
+
+
+
+    }
 }
---
+++ src/Zahin.java
@@ -0,0 +1,8 @@
+public class Zahin {
+    public static void main(String[] args) {
+        System.out.println("My name is Zahin Tasnin");
+
+    }
+
+
+}
