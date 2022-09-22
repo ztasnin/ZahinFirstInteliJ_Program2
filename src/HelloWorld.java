ad9061761bb8712913f230c075b27b951e1163e7
Zahin Tasnin <ztasnin8573@bths.edu>
2022-09-21 21:51:38 -0400

    New line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
         System.out.println("Please enter your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name);
 
 
 
